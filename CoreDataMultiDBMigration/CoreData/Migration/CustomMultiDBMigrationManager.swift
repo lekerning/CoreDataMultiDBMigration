0d618f3827bb283e5524909a8a8e524e7add6d06
Jason Zhang <jason.zhang@autodesk.com>
2016-09-11 16:13:23 +0800

    Add  V1 version info

--- CoreDataMultiDBMigration/CoreData/Migration/CustomMultiDBMigrationManager.swift
+++ CoreDataMultiDBMigration/CoreData/Migration/CustomMultiDBMigrationManager.swift
@@ -21,11 +21,19 @@ extension CustomMultiDBMigrationManager: MultiDBMigrationWrapperDelegate {
     }
 
     func migrationRoadMap() -> VersionMigrationManager {
-        let versionInfo1: VersionMigrationAble = DataBaseInfo.Server(ModelVersionInfo(currentModel: "Server.mom", mappingModel: nil, destinationModel: nil))
+        let versionInfoServer: VersionMigrationAble = DataBaseInfo.Server(ModelVersionInfo(currentModel: "Server.mom", mappingModel: nil, destinationModel: nil))
         let versionInfo1Local: VersionMigrationAble = DataBaseInfo.Local(ModelVersionInfo(currentModel: "Local.mom", mappingModel: nil, destinationModel: nil))
-        let singleManage1_1 = VersionMigrationManager(dbs: [versionInfo1, versionInfo1Local])
+        let singleManage1_0 = VersionMigrationManager(dbs: [versionInfoServer, versionInfo1Local])
 
-        return singleManage1_1
+        let versionInfoServer1: VersionMigrationAble = DataBaseInfo.Server(ModelVersionInfo(currentModel: "Server_V1.mom", mappingModel: nil, destinationModel: nil))
+        let versionInfo1Local1: VersionMigrationAble = DataBaseInfo.Local(ModelVersionInfo(currentModel: "Local_V1.mom", mappingModel: nil, destinationModel: nil))
+        let singleManage1_1 = VersionMigrationManager(dbs: [versionInfoServer1, versionInfo1Local1])
+
+
+        singleManage1_0.nextVersionMigrationManager = singleManage1_1
+
+
+        return singleManage1_0
 
     }
 
